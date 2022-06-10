30635f4e0ad7ef4cd00a2805499b39168e7a26d8
+ <+>
2022-06-10 10:24:09 +0300

    commit

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet<=400){
+    return 'This one is on me!';
+  }
+  else if ( feet<=2500){
+    return 'I will gladly take your thirty bucks.,$ I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.'
+  }
+}
+  
+
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? ("Ok,sounds good."):("No go.");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+    return "Thank you so much";
+    case "not as generous":
+    return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  
