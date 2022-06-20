4e95f78d5883ebf73c292d11c0aa1369e4481351
+ <+>
2022-06-20 14:30:11 +0300

    commit

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
+
 function scuberGreetingForFeet(feet){
-  if (feet<=400){
+  if(feet <= 400){
+    return 'This one is on me!'
+  } else if (1999 < feet && feet < 2500){
+      return "I will gladly take your thirty bucks."
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
     return 'This one is on me!';
-  }
-  else if (distance > 2000 && distance <2500){
-    return 'I will gladly take your thirty bucks.,$ I will gladly take your thirty bucks.';
-  }
-  else {
-    return 'No can do.'
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do."
+    return "No can do.";
   }
 }
-  
-
-
+  }
+}
 
 function ternaryCheckCity(city){
-  // Write your code here!
-  return city === "NYC" ? ("Ok,sounds good."):("No go.");
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip(tip){
-  switch(tip){
-    case "generous":
-    return "Thank you so much";
-    case "not as generous":
-    return "Thank you.";
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+      return 'Thank you.';
     default:
-      return "Bye.";
+      return 'Bye.'
+      return 'Bye.';
   }
 }
-
-  
+}
+}
